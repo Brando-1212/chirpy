745aa89439f38a5a880e25ae3dcfa1dda72b8fe5
Brando-1212 <birkelobrandon@gmail.com>
2026-03-03 11:59:33 -0600

    B. Added healthz, metrics, and reset handlers

--- main.go
+++ main.go
@@ -3,20 +3,30 @@ package main
 import(
 	"log"
 	"net/http"
+	"sync/atomic"
+	"fmt"
+	
 )
 
 
-
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
 
 
 func main() {
 	const port = "8080"
 	const filepathRoot = "."
+	apiCfg := apiConfig{}
 
 	
 	mux := http.NewServeMux()
 	file := http.Dir(filepathRoot)
-	mux.Handle("/", http.FileServer(file))
+
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(file))))
+	mux.HandleFunc("GET /api/healthz", handlerReadiness)
+	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
 	server := &http.Server{
 		Addr : ":" + port,
 		Handler : mux,
@@ -26,4 +36,40 @@ func main() {
 	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(server.ListenAndServe())
 
+}
+
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
+func (cfg *apiConfig) handlerMetrics (w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf(`
+<html>
+
+<body>
+	<h1>Welcome, Chirpy Admin</h1>
+	<p>Chirpy has been visited %d times!</p>
+</body>
+
+</html>
+	`, cfg.fileserverHits.Load())))
+}
+
+func (cfg *apiConfig) handlerReset (w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	cfg.fileserverHits.Store(0)
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("file server hits reset to 0"))
+	
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
 }
