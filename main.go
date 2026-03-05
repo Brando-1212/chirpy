49dd6aa35bfb584ab5dc9a161733a3bb72c6e82a
Brando-1212 <birkelobrandon@gmail.com>
2026-03-05 10:46:47 -0600

    C. Added validate_chirp function handler

--- main.go
+++ main.go
@@ -3,9 +3,7 @@ package main
 import(
 	"log"
 	"net/http"
-	"sync/atomic"
-	"fmt"
-	
+	"sync/atomic"	
 )
 
 
@@ -27,6 +25,7 @@ func main() {
 	mux.HandleFunc("GET /api/healthz", handlerReadiness)
 	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
 	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
+	mux.HandleFunc("POST /api/validate_chirp", handlerValidateChirp)
 	server := &http.Server{
 		Addr : ":" + port,
 		Handler : mux,
@@ -38,34 +37,12 @@ func main() {
 
 }
 
-func handlerReadiness(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(http.StatusText(http.StatusOK)))
-}
 
-func (cfg *apiConfig) handlerMetrics (w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(fmt.Sprintf(`
-<html>
 
-<body>
-	<h1>Welcome, Chirpy Admin</h1>
-	<p>Chirpy has been visited %d times!</p>
-</body>
 
-</html>
-	`, cfg.fileserverHits.Load())))
-}
 
-func (cfg *apiConfig) handlerReset (w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
-	cfg.fileserverHits.Store(0)
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("file server hits reset to 0"))
-	
-}
+
+
 
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -73,3 +50,5 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 		next.ServeHTTP(w, r)
 	})
 }
+
+
